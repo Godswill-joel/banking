cf32961fdeb6669a5e2fd3a2f4a410f4d53a77bc
Godswill-joel <ge42217@gmail.com>
2025-10-25 14:02:28 +0100

    feat: Restore FeaturedIn component with logos from various sources

--- src/app/privateriver/page.tsx
+++ src/app/privateriver/page.tsx
@@ -1,7 +1,7 @@
 import ContactForm from "../../Components/Pages/privateriver/Contact";
 import Privateriver from "../../Components/Pages/privateriver/Privateriver";
 import Personalized from "../../Components/Pages/privateriver/Personalized";
-import FeaturedIn from "@/Components/Pages/privateriver/FeaturedIn";
+import FeaturedIn from "@/Components/Pages/FeaturedIn";
 import WorldClassSecurity from "../../Components/Pages/privateriver/Worldclass";
 
 
