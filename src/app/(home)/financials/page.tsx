21d905f7a929e05e8156cec9aa9842071ab3a981
Godswill-joel <ge42217@gmail.com>
2026-01-05 15:28:52 +0100

    Add FinancialMetricsSection component and integrate it into the financials page

--- src/app/(home)/financials/page.tsx
+++ src/app/(home)/financials/page.tsx
@@ -1,9 +1,11 @@
 import FinancialsHero from "@/Components/Pages/financials/financialsHero";
+import FinancialMetricsSection from "@/Components/Pages/financials/metricSectioin";
 
 export default function page(){
     return (
         <>
            <FinancialsHero />
+           <FinancialMetricsSection />
         </>
     )
 }
